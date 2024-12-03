348fbc1a0b954a421ac0ea1f46c5b2c0a005e8b9
kakao0011 <sole68victor@gmail.com>
2024-12-03 14:53:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Caravans.nl</a>
+          Caravans.nl
         </h1>
       </main>
     </div>
